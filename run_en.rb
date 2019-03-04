2d7962162bde7175f8e49156ce101e1645954548
Thomas Bnt <thomasbnt@protonmail.com>
2019-03-04 13:04:26 +0100

    Youhow! It's time for to code Ruby + Guide
    Too many uppercase.

--- run_en.rb
+++ run_en.rb
@@ -1,16 +1,16 @@
-MaxChiffre = 20
-NumberToGuess = Random.new.rand(MaxChiffre)
-MaxChance = Random.new.rand(2..6)
+Max_number = 20
+Number_to_guess = Random.new.rand(Max_number)
+Max_chance = Random.new.rand(2..6)
 a = nil
-puts "Enter your number between 0 and #{MaxChiffre} included, you only have #{MaxChance} chances."
-for num in 1..MaxChance
-  next if a == NumberToGuess
+puts "Enter a number between 0 and #{Max_number} included, you only have to #{Max_chance} chances."
+for num in 1..Max_chance
+  next if a == Number_to_guess
   a = gets.chomp.to_i
-  if a > NumberToGuess
-    puts "Number too big"
-  elsif a < NumberToGuess
-    puts "Number too small"
+  if a > Number_to_guess
+    puts "Number to big 🔽"
+  elsif a < Number_to_guess
+    puts "Number to small 🔼"
   end
 end
-puts "Congratulations, you've found it !" if a == NumberToGuess
-puts "Pity ! It was #{NumberToGuess}. " if a != NumberToGuess
+puts "Well done, you've found it! 👏" if a == Number_to_guess
+puts "Too bad! It was #{Number_to_guess}. " if a != Number_to_guess
